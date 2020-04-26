6116308c2710190e71d9523fe6ff238d1d7e6e41
T-Dimi <thomas.dimitriadis@icloud.com>
2020-04-25 20:05:20 -0700

    Adding weather details to current weather section

--- src/components/SimpleReport.jsx
+++ src/components/SimpleReport.jsx
@@ -5,10 +5,10 @@ import moment from "moment";
 import "./css/simplereport.css";
 
 const SimpleReport = ({ weatherReport }) => {
+  console.log("weatherReport: ", weatherReport);
   const [windSpeed, setWindSpeed] = useState("");
   const [weatherDescription, setWeatherDescription] = useState("");
   const [currentTemp, setCurrentTemp] = useState("");
-  const [weatherIcon, setWeatherIcon] = useState(undefined);
   const [currentDay, setCurrentDay] = useState("");
 
   const handleWindSpeed = useCallback(() => {
@@ -16,44 +16,35 @@ const SimpleReport = ({ weatherReport }) => {
     let cardinalWindDirection = "";
 
     if (windDirection < 11.25 || windDirection > 348.75)
-      cardinalWindDirection = " N";
+      cardinalWindDirection = "N ";
     else if (windDirection > 11.25 && windDirection < 78.75)
-      cardinalWindDirection = " NE";
+      cardinalWindDirection = "NE ";
     else if (windDirection > 78.75 && windDirection < 123.75)
-      cardinalWindDirection = " E";
+      cardinalWindDirection = "E ";
     else if (windDirection > 123.75 && windDirection < 168.75)
-      cardinalWindDirection = " SE";
+      cardinalWindDirection = "SE ";
     else if (windDirection > 168.75 && windDirection < 213.75)
-      cardinalWindDirection = " S";
+      cardinalWindDirection = "S ";
     else if (windDirection > 213.75 && windDirection < 258.75)
-      cardinalWindDirection = " SW";
+      cardinalWindDirection = "SW ";
     else if (windDirection > 258.75 && windDirection < 281.25)
-      cardinalWindDirection = " W";
+      cardinalWindDirection = "W ";
     else if (windDirection > 281.25 && windDirection < 348.75)
-      cardinalWindDirection = " NW";
+      cardinalWindDirection = "NW ";
 
     weatherReport &&
       setWindSpeed(
-        Math.round(weatherReport.wind_speed) + " MPH" + cardinalWindDirection
+        cardinalWindDirection + Math.round(weatherReport.wind_speed) + " MPH"
       );
-  }, [weatherReport]);
-
-  const getWeatherIcon = useCallback(() => {
-    weatherReport &&
-      fetch(
-        `http://openweathermap.org/img/wn/${weatherReport.weather[0].icon}@2x.png`
-      ).then((data) => {
-        setWeatherIcon(data.url);
-      });
   }, [weatherReport]);
 
   useEffect(() => {
     handleWindSpeed();
-    getWeatherIcon();
+
     setCurrentTemp(weatherReport && weatherReport.temp + " \xB0F");
     setWeatherDescription(weatherReport && weatherReport.weather[0].main);
     setCurrentDay(moment().format("dddd").toUpperCase());
-  }, [handleWindSpeed, getWeatherIcon, weatherReport]);
+  }, [handleWindSpeed, weatherReport]);
 
   return (
     <div className="simple-report-grid-container">
@@ -61,15 +52,24 @@ const SimpleReport = ({ weatherReport }) => {
         <div className="simple-report-current-weather">Current Weather</div>
         <div className="simple-report-line"></div>
       </div>
-      <div className="simple-report-image-container">
-        <img
-          className="simple-report-icon"
-          src={weatherIcon}
-          alt="React Logo"
-        />
-      </div>
-      <div className="simple-report-wind-container">
-        <div className="simple-report-wind">{windSpeed}</div>
+      <div className="simple-report-details-container">
+        <div>
+          <span className="simple-report-detail">Humidity: </span>
+          <span className="simple-report-detail-data">
+            {weatherReport && weatherReport.humidity + "%"}
+          </span>
+        </div>
+        <div className="simple-report-wind">
+          <span className="simple-report-detail">Wind: </span>
+          <span className="simple-report-detail-data">{windSpeed}</span>
+        </div>
+        <div className="simple-report-wind">
+          <span className="simple-report-detail">Pressure: </span>
+          <span className="simple-report-detail-data">
+            {weatherReport &&
+              (weatherReport.pressure * 0.02953).toFixed(2) + " in"}
+          </span>
+        </div>
       </div>
       <div className="simple-report-current-day-container">
         <div className="simple-report-current-day">{currentDay}</div>
