5bb4baaafcb8475e112264b8c38ce1d5e6dd9195
T-Dimi <thomas.dimitriadis@icloud.com>
2020-05-11 22:02:13 -0700

    Adding the rest of the hourly weather icons

--- src/components/HourlyForecast.jsx
+++ src/components/HourlyForecast.jsx
@@ -1,34 +1,33 @@
-import React, { useState } from "react";
+import React from 'react';
 
-import moment from "moment";
+import moment from 'moment';
 
-import { icons } from "../constants.js";
+import { icons } from '../constants.js';
 
-import "./css/hourlyForecast.css";
+import './css/hourlyForecast.css';
 
 const HourlyForecast = ({ weatherReport }) => {
-  console.log(icons[0]["01d"]);
-
   const hourly = () => {
     return (
       weatherReport.hourly &&
       weatherReport.hourly.map((val, i) => {
+        let weatherIcon = val.weather[0].icon;
+        let time = moment.unix(val.dt).format('h A');
+
         if (i >= 14) {
           return null;
         }
-        let time = moment.unix(val.dt).format("h A");
-        console.log("val.weather[0].icon: ", val.weather[0].icon);
 
         return (
           <div className={`hourly-forecast-${i + 1}`} key={i}>
-            <div className="hourly-forecast-time">{time}</div>
+            <div className='hourly-forecast-time'>{time}</div>
             <img
-              className="hourly-forecast-icon"
-              src={icons[6]["04d"]}
-              alt="weatherIcon"
+              className='hourly-forecast-icon'
+              src={icons[`${weatherIcon}`]}
+              alt='weatherIcon'
             ></img>
-            <div className="hourly-forecast-temp">
-              {Math.round(val.temp) + "\xB0"}
+            <div className='hourly-forecast-temp'>
+              {Math.round(val.temp) + '\xB0'}
             </div>
           </div>
         );
@@ -37,10 +36,10 @@ const HourlyForecast = ({ weatherReport }) => {
   };
 
   return (
-    <div className="hourly-forecast-grid-container">
-      <div className="hourly-forecast-header">
-        <div className="hourly-forecast-title">Forecast</div>
-        <div className="hourly-forecast-line"></div>
+    <div className='hourly-forecast-grid-container'>
+      <div className='hourly-forecast-header'>
+        <div className='hourly-forecast-title'>Forecast</div>
+        <div className='hourly-forecast-line'></div>
       </div>
       {hourly()}
     </div>
--- src/constants.js
+++ src/constants.js
@@ -1,25 +1,33 @@
-import sunIcon from "./assets/sunIcon.png";
-import nightSunIcon from "./assets/nightSunIcon.png";
-import nightBrokenClouds from "./assets/nightBrokenClouds.png";
-import icon1 from "./assets/icon.png";
+import sunIcon from './assets/sunIcon.png';
+import nightSunIcon from './assets/nightSunIcon.png';
+import nightBrokenClouds from './assets/nightBrokenClouds.png';
+import fewCloudsIcon from './assets/fewCloudsIcon.png';
+import nightFewCloudsIcon from './assets/nightFewCloudsIcon.png';
+import scatteredCloudsIcon from './assets/scatteredCloudsIcon.png';
+import showerRainIcon from './assets/showerRainIcon.png';
+import rainIcon from './assets/rainIcon.png';
+import nightRainIcon from './assets/nightRainIcon.png';
+import thunderstormIcon from './assets/thunderstormIcon.png';
+import snowIcon from './assets/snowIcon.png';
+import windIcon from './assets/windIcon.png';
 
-export const icons = [
-  { ["01d"]: sunIcon },
-  { ["01n"]: nightSunIcon },
-  { ["02d"]: 3 },
-  { ["02n"]: 3 },
-  { ["03d"]: 3 },
-  { ["03n"]: 3 },
-  { ["04d"]: nightBrokenClouds },
-  { ["04n"]: nightBrokenClouds },
-  { ["09d"]: 3 },
-  { ["09n"]: 3 },
-  { ["10d"]: 3 },
-  { ["10n"]: 3 },
-  { ["11d"]: 3 },
-  { ["11n"]: 3 },
-  { ["13d"]: 3 },
-  { ["13n"]: 3 },
-  { ["50n"]: 3 },
-  { ["50d"]: 3 }
-];
+export const icons = {
+  '01d': sunIcon,
+  '01n': nightSunIcon,
+  '02d': fewCloudsIcon,
+  '02n': nightFewCloudsIcon,
+  '03d': scatteredCloudsIcon,
+  '03n': scatteredCloudsIcon,
+  '04d': nightBrokenClouds,
+  '04n': nightBrokenClouds,
+  '09d': showerRainIcon,
+  '09n': showerRainIcon,
+  '10d': rainIcon,
+  '10n': nightRainIcon,
+  '11d': thunderstormIcon,
+  '11n': thunderstormIcon,
+  '13d': snowIcon,
+  '13n': snowIcon,
+  '50n': windIcon,
+  '50d': windIcon,
+};
