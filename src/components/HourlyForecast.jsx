6a162d6e4efcc2e6ea8eb1d320c99c4b261b1429
T-Dimi <thomas.dimitriadis@icloud.com>
2020-05-11 21:34:47 -0700

    Updating hourly weather icons

--- src/components/HourlyForecast.jsx
+++ src/components/HourlyForecast.jsx
@@ -1,32 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import moment from 'moment';
+import moment from "moment";
 
-import { icons } from '../constants.js';
+import { icons } from "../constants.js";
 
-import './css/hourlyForecast.css';
+import "./css/hourlyForecast.css";
 
 const HourlyForecast = ({ weatherReport }) => {
-  const [icon, setIcon] = useState();
-
-  console.log(icons[0]['01d']);
+  console.log(icons[0]["01d"]);
 
   const hourly = () => {
     return (
       weatherReport.hourly &&
       weatherReport.hourly.map((val, i) => {
         if (i >= 14) {
-          return;
+          return null;
         }
-        let time = moment.unix(val.dt).format('h A');
-        console.log('val.weather[0].icon: ', val.weather[0].icon);
+        let time = moment.unix(val.dt).format("h A");
+        console.log("val.weather[0].icon: ", val.weather[0].icon);
 
         return (
           <div className={`hourly-forecast-${i + 1}`} key={i}>
-            <div className='hourly-forecast-time'>{time}</div>
-            <img className='hourly-forecast-icon' src={icons[0]['01d']}></img>
-            <div className='hourly-forecast-temp'>
-              {Math.round(val.temp) + '\xB0'}
+            <div className="hourly-forecast-time">{time}</div>
+            <img
+              className="hourly-forecast-icon"
+              src={icons[6]["04d"]}
+              alt="weatherIcon"
+            ></img>
+            <div className="hourly-forecast-temp">
+              {Math.round(val.temp) + "\xB0"}
             </div>
           </div>
         );
@@ -35,10 +37,10 @@ const HourlyForecast = ({ weatherReport }) => {
   };
 
   return (
-    <div className='hourly-forecast-grid-container'>
-      <div className='hourly-forecast-header'>
-        <div className='hourly-forecast-title'>Forecast</div>
-        <div className='hourly-forecast-line'></div>
+    <div className="hourly-forecast-grid-container">
+      <div className="hourly-forecast-header">
+        <div className="hourly-forecast-title">Forecast</div>
+        <div className="hourly-forecast-line"></div>
       </div>
       {hourly()}
     </div>
--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,25 @@
-import icon1 from './assets/icon.png';
+import sunIcon from "./assets/sunIcon.png";
+import nightSunIcon from "./assets/nightSunIcon.png";
+import nightBrokenClouds from "./assets/nightBrokenClouds.png";
+import icon1 from "./assets/icon.png";
 
 export const icons = [
-  { ['01d']: icon1 },
-  { ['01n']: 3 },
-  { ['02d']: 3 },
-  { ['02n']: 3 },
+  { ["01d"]: sunIcon },
+  { ["01n"]: nightSunIcon },
+  { ["02d"]: 3 },
+  { ["02n"]: 3 },
+  { ["03d"]: 3 },
+  { ["03n"]: 3 },
+  { ["04d"]: nightBrokenClouds },
+  { ["04n"]: nightBrokenClouds },
+  { ["09d"]: 3 },
+  { ["09n"]: 3 },
+  { ["10d"]: 3 },
+  { ["10n"]: 3 },
+  { ["11d"]: 3 },
+  { ["11n"]: 3 },
+  { ["13d"]: 3 },
+  { ["13n"]: 3 },
+  { ["50n"]: 3 },
+  { ["50d"]: 3 }
 ];
