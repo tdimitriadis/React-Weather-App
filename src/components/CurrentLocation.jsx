0c9f27b16df680e45a9f6ba8adeb6823ee5bba1c
T-Dimi <thomas.dimitriadis@icloud.com>
2020-05-12 19:05:24 -0700

    Updating current location to use new weather icon pack

--- src/components/CurrentLocation.jsx
+++ src/components/CurrentLocation.jsx
@@ -1,44 +1,33 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React from "react";
 
-import Button from '../components/Button.jsx';
-import './css/currentLocation.css';
+import Button from "../components/Button.jsx";
+import { SEVEN_DAY_ICONS } from "../constants.js";
+
+import "./css/currentLocation.css";
 
 const CurrentLocation = ({ location, weatherReport, click }) => {
-  const [weatherIcon, setWeatherIcon] = useState(undefined);
-
-  const getWeatherIcon = useCallback(() => {
-    weatherReport &&
-      fetch(
-        `http://openweathermap.org/img/wn/${weatherReport.weather[0].icon}@2x.png`,
-      ).then(data => {
-        setWeatherIcon(data.url);
-      });
-  }, [weatherReport]);
-
-  useEffect(() => {
-    getWeatherIcon();
-  }, [getWeatherIcon, weatherReport]);
+  const weatherIcon = weatherReport?.weather[0].icon;
 
   return (
-    <div className='current-location-grid-container'>
-      <div className='current-location-image'>
+    <div className="current-location-grid-container">
+      <div className="current-location-image">
         <img
-          className='current-location-icon'
-          src={weatherIcon}
-          alt='Weather Icon'
+          className="current-location-icon"
+          src={SEVEN_DAY_ICONS[`${weatherIcon}`]}
+          alt="Weather Icon"
         />
       </div>
-      <div className='current-location-city'>
-        <span className='current-location-city-name'>
+      <div className="current-location-city">
+        <span className="current-location-city-name">
           {location && location.city}
         </span>
-        <div className='current-location-state-name'>
+        <div className="current-location-state-name">
           City in {location && location.statename}
         </div>
       </div>
-      <div className='current-location-change-location'>
-        <span className='current-location-button'>
-          <Button onClick={click} value='Change Location' />
+      <div className="current-location-change-location">
+        <span className="current-location-button">
+          <Button onClick={click} value="Change Location" />
         </span>
       </div>
     </div>
--- src/components/SevenForecast.jsx
+++ src/components/SevenForecast.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
+import React from "react";
 
-import moment from 'moment';
+import moment from "moment";
 
-import { SEVEN_DAY_ICONS } from '../constants.js';
+import { SEVEN_DAY_ICONS } from "../constants.js";
 
-import './css/sevenForecast.css';
+import "./css/sevenForecast.css";
 
 const SevenForecast = ({ weatherReport }) => {
-  console.log('weatherReport: ', weatherReport.daily);
+  console.log("weatherReport: ", weatherReport.daily);
   const daily = () => {
     return (
       weatherReport.daily &&
       weatherReport.daily.map((val, i) => {
         let weatherIcon = val.weather[0].icon;
-        let day = moment.unix(val.dt).format('dddd');
+        let day = moment.unix(val.dt).format("dddd");
         let description = val.weather[0].description;
 
         if (i >= 7) {
@@ -24,17 +24,17 @@ const SevenForecast = ({ weatherReport }) => {
           <div className={`seven-forecast-${i + 1}`} key={i}>
             {day}
             <div>
-              <div className='seven-forecast-description'>
+              <div className="seven-forecast-description">
                 {description.replace(/^./, description[0].toUpperCase())}
               </div>
               <img
-                className='seven-forecast-icon'
+                className="seven-forecast-icon"
                 src={SEVEN_DAY_ICONS[`${weatherIcon}`]}
-                alt='weatherIcon'
+                alt="weatherIcon"
               ></img>
             </div>
-            <div className='seven-forecast-temp'>
-              {Math.round(val.temp.day) + '\xB0'}
+            <div className="seven-forecast-temp">
+              {Math.round(val.temp.day) + "\xB0"}
             </div>
           </div>
         );
@@ -42,18 +42,7 @@ const SevenForecast = ({ weatherReport }) => {
     );
   };
 
-  return (
-    <div className='seven-forecast-grid-container'>
-      {daily()}
-      {/* <div className='seven-forecast-one'>Sunday</div>
-      <div className='seven-forecast-two'>Monday</div>
-      <div className='seven-forecast-three'>Tuesday</div>
-      <div className='seven-forecast-four'>Wednesday</div>
-      <div className='seven-forecast-five'>Thursday</div>
-      <div className='seven-forecast-six'>Friday</div>
-      <div className='seven-forecast-seven'>Saturday</div> */}
-    </div>
-  );
+  return <div className="seven-forecast-grid-container">{daily()}</div>;
 };
 
 export default SevenForecast;
